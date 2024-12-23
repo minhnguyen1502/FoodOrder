9c1591b26b7e4c3ba95db574ab4e0b2270ac9db2
minhnguyen1502 <minhnguyen010502@gmail.com>
2024-12-23 23:03:24 +0700

    first commit

---
+++ app/src/main/java/com/example/foodorder/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.foodorder.ui
+
+import com.example.foodorder.base.BaseActivity
+import com.example.foodorder.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    override fun initView() {
+    }
+
+    override fun bindView() {
+    }
+}
